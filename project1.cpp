a6a306b34709e77c2cf7ef72e45e91eb51ed0518
mishaalmallick <mishaalmallick@gmail.com>
2025-01-30 01:57:06 -0500

    Update project1.cpp

--- project1.cpp
+++ project1.cpp
@@ -581,6 +581,29 @@ int main(int argc, char* argv[]){
     getline(cin, comment);
     cin >> map_or_list;
     cout << map_or_list << endl;
+        if(map_or_list == 'M'){
+cout << "x" << endl;
+ 
+    }
+    if(map_or_list == 'L'){
+ cout << "h" << endl;
+    }
+    
+    Map Game = Map(Full_Map, starting_location, maximum_number_rows, maximum_number_rows, ending_location,
+    v, ss, ms, hunt_ordering);
+    if(v){
+        Game.Verbose();
+    }
+    if(statistics){
+        Game.Stats_Print();
+    }
+    if(map_or_list == 'M' && path){
+        Game.Treasure_Map_Print();
+    }
+    if(map_or_list == 'L' && path){
+        Game.Coordinate_Location();
+    }
+
 
     return 0; 
 
